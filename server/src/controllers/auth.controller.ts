ee4e074d8cde93d3ca8de1ae39a5b7c8ef08f6c5
Your Name <caihebatista@gmail.com>
2024-09-08 21:36:44 -0300

    Feat: auth controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,20 +1,42 @@
 import type { PrismaClient } from "@prisma/client";
 import { prisma } from "../lib/prisma.client";
-import fastify from "fastify";
-import { genSalt, compare } from 'bcrypt'
-import type { RegisterRequest } from "../lib/types/dtos";
+import { type FastifyInstance, type FastifyPluginAsync } from "fastify";
+import { genSalt, hash, compare } from 'bcrypt';
+import type { LoginRequest, RegisterRequest } from "../lib/types/dtos";
 import type { Body } from "../lib/types/utils";
-
-export const authController = fastify()
-    .post<Body<RegisterRequest>>('/register', async (req) => {
-        const { email, password, name } = req.body
-        // const 
+import { AppError } from '@/lib/appError';
+export const authController: FastifyPluginAsync = async (fastify: FastifyInstance) => {
+    fastify.post<Body<RegisterRequest>>('/auth/register', async (req, reply) => {
+        const { email, password, name } = req.body;
+        const salt = await genSalt(10);
+        const hashedPassword = await hash(password, salt);
+        if (await prisma.user.findUnique({ where: { email } })) {
+            throw new AppError('User already exists');
+        }
         const user = await prisma.user.create({
             data: {
                 email,
-                password,
-                
+                password: hashedPassword,
+                name
             }
-        })
-        return user
-    })
+        });
+        const token = fastify.jwt.sign({ id: user.id });
+        return { token };
+    });
+
+    fastify.post<Body<LoginRequest>>('/auth/login', async (req, reply) => {
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({
+            where: { email }
+        });
+        if (!user) {
+            throw new AppError('User not found');
+        }
+        const isPasswordValid = await compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new AppError('Invalid password');
+        }
+        const token = fastify.jwt.sign({ id: user.id });
+        return { token };
+    });
+};
