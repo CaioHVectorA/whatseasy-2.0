1dde000e8181b351b488dff0efa232d7f3708618
Your Name <caihebatista@gmail.com>
2024-09-08 21:37:49 -0300

    Feat: some abstractions and decorators in index js

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,65 @@
 import Fastify from 'fastify'
 import websocket from '@fastify/websocket'
 import { main } from './src/lib/ws'
+import jwt, { } from '@fastify/jwt'
+import { userController } from '@/controllers/user.controller'
+import { authController } from '@/controllers/auth.controller'
+import { AppError } from '@/lib/appError'
+import { prisma } from '@/lib/prisma.client'
 const fastify = Fastify({
-    logger: true,
+    logger: true
 })
-
+declare module 'fastify' {
+    interface FastifyRequest {
+      me?: any;
+    }
+  }
 fastify.register(websocket)
+fastify.register(jwt, { secret: process.env.JWT_SECRET || "secret" })
+fastify.register(authController)
+fastify.setErrorHandler(async (error, request, reply) => {
+    if (error instanceof AppError) {
+        reply.status(error.statusCode).send({
+            status: error.statusCode,
+            message: error.message,
+            isOperational: true,
+            // stack: error.stack
+        })
+    }
+    reply.status(error.statusCode || 500).send({ ...error, isOperational: false })
+})
+fastify.addHook("onRequest", async (request, reply) => {
+    if (request.url.startsWith('/auth')) return
+    try {
+        await request.jwtVerify()
+    } catch (err) {
+        reply.send(err)
+    }
+})
+fastify.addHook("onRequest", async (request, reply) => {
+    try {
+        // Supondo que request.user seja um JWT string
+        const user = request.user as { id: string };
+        console.log(user, request.user)
+        // Busca o usuário pelo ID do JWT
+        request.me = await prisma.user.findUnique({
+            where: { id: user.id },
+        });
 
-//   socket.on('message', message => {
-//     const messageString = message.toString()
-//     console.log('Received:', messageString)
-    
-//     if (messageString.toLowerCase() === 'ping') {
-//       socket.send('pong')
-//     }
-//   })
+        // Se o usuário não for encontrado, retorna erro
+        if (!request.me) {
+            return reply.status(404).send({ error: "Usuário não encontrado!" });
+        }
+    } catch (err) {
+        console.log({ err })
+        // Se der erro, retorna erro 500
+        return reply.status(500).send({ error: "Erro encontrando usuário!" });
+    }
+})
+fastify.register(userController)
+
 fastify.register(async function (fastify) {
-    fastify.get('/ws', { websocket: true }, (socket) => {
+    fastify.get('/ws', { websocket: true }, (socket, req) => {
         socket.on('connection', () => console.log('Client connected.'))
         socket.on('message', main(socket))
         // socket.on('')
