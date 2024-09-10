4d1b4c8f8e50853e528b00fae22fad9d0a0704fb
Your Name <caihebatista@gmail.com>
2024-09-10 12:46:27 -0300

    Fix: adap sockets

--- src/lib/ws/index.ts
+++ src/lib/ws/index.ts
@@ -1,14 +1,16 @@
 import { type WebSocket } from '@fastify/websocket'
-import { ClientSignals } from './signals'
+import { ClientSignals, ServerSignals, type WebSocketRequest } from './signals'
 import { createClient } from '../wpp/Client'
-import type { RawData } from 'ws'
 import { handleConnectionSockClosure } from '../wpp/handleConection'
 import { connect } from '../wpp/connect'
+import type { RawData } from 'ws'
 export function main(socket: WebSocket) {
     return async (data: RawData) => {
         // console.log(data)
         // socket.on('ping',)
-        if (data.toString() !== 'new-client') return
+        const request = JSON.parse(data.toString()) as WebSocketRequest
+        console.log({ request })
+        if (request.event !== ServerSignals.NEW_CLIENT) return
             const client = await createClient()
             socket.send(JSON.stringify({ event: ClientSignals.CLIENT_UPDATE, message: "Seu cliente foi criado! Seu qrcode será enviado em breve", clientUUid: client.clientUUid }))
             client.sock.ev.on('connection.update', handleConnectionSockClosure(client.sock, socket, connect, client.clientUUid))
--- src/lib/ws/signals.ts
+++ src/lib/ws/signals.ts
@@ -9,3 +9,18 @@ export enum ClientSignals {
     CLIENT_SUCESS = 'client.success',
     CLIENT_FAIL = 'client.fail',
 }
+
+export type WebSocketResponse = {
+    event: typeof ClientSignals[keyof typeof ClientSignals],
+    data: any,
+    uuid: string,
+    message: string,
+    send_date: string
+}
+
+export type WebSocketRequest = {
+    event: typeof ServerSignals[keyof typeof ServerSignals],
+    data: any,
+    uuid: string,
+    send_date: string
+}
