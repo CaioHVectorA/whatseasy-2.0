fbb1d286ee9d46afac52362d65c8497bc24a8ca5
Your Name <caihebatista@gmail.com>
2024-09-20 22:33:13 -0300

    Refactor: Update WebSocket main function to include client tracking
    - Updated the main function in the WebSocket module to include client tracking.
    - Added a new parameter "clients" to the main function to store the list of connected clients.
    - Modified the createClient function call to include the request UUID and the clients list.
    - Added a console log message to indicate when a client is created.

--- src/lib/ws/index.ts
+++ src/lib/ws/index.ts
@@ -1,18 +1,21 @@
 import { type WebSocket } from '@fastify/websocket'
 import { ClientSignals, ServerSignals, type WebSocketRequest } from './signals'
-import { createClient } from '../wpp/Client'
+import { createClient, type Client } from '../wpp/Client'
 import { handleConnectionSockClosure } from '../wpp/handleConection'
 import { connect } from '../wpp/connect'
 import type { RawData } from 'ws'
-export function main(socket: WebSocket) {
+// const clients: Client[] = []
+export function main(socket: WebSocket, clients: Client[]) {
     return async (data: RawData) => {
         // console.log(data)
         // socket.on('ping',)
         const request = JSON.parse(data.toString()) as WebSocketRequest
         console.log({ request })
         if (request.event !== ServerSignals.NEW_CLIENT) return
-            const client = await createClient()
+            const client = await createClient(request.uuid, clients)
+            console.log('Cliente criado!')
             socket.send(JSON.stringify({ event: ClientSignals.CLIENT_UPDATE, message: "Seu cliente foi criado! Seu qrcode será enviado em breve", clientUUid: client.clientUUid }))
             client.sock.ev.on('connection.update', handleConnectionSockClosure(client.sock, socket, connect, client.clientUUid))
     }
 }
+
