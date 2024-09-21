260f852fabf0b0597922c1f34f11def9b241a650
Your Name <caihebatista@gmail.com>
2024-09-20 22:32:23 -0300

    Feat: Add schedules table to database schema

---
+++ prisma/migrations/20240920043806_add_schedules/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "message" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "time" DATETIME NOT NULL,
+    CONSTRAINT "schedules_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
