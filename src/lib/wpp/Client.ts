aa7cc097e8f4a0564cfae93d8c52a69c5ba36a52
Your Name <caihebatista@gmail.com>
2024-09-20 22:33:02 -0300

    Refactor: Improve client creation logic and add in-memory client tracking

--- src/lib/wpp/Client.ts
+++ src/lib/wpp/Client.ts
@@ -2,17 +2,20 @@ import makeWASocket, { getChatId } from "@whiskeysockets/baileys";
 import { connect } from "./connect";
 import { INCLUDED_CHATS } from "../../helpers/consts";
 import type { ModifiedSock } from "../types/modified.sock.type";
-type Client = {
+export type Client = {
     sock: ModifiedSock;
     clientUUid: string,
     init: number,
     connected: boolean,
 }
-
-export async function createClient(): Promise<Client> {
-    const clientUUid = crypto.randomUUID()
+// PERSIST!!
+export async function createClient(uuid: string, clients: Client[]): Promise<Client> {
+    const inMemoClient = clients.find((c) => c.clientUUid === uuid);
+    if (inMemoClient) {
+        return inMemoClient
+    };
     const init = Date.now();
-    const sock = await connect(clientUUid);
+    const sock = await connect(uuid);
     sock.ev.on('messages.upsert', async (m) => {
         if (m.messages[0].message?.conversation === "Olá, mundo!" || !INCLUDED_CHATS.includes(getChatId({ ...m.messages[0].key })) ) return;
         //@ts-ignore
@@ -26,10 +29,13 @@ export async function createClient(): Promise<Client> {
           text: response,
         });
     });
-    return {
+    const client = {
         sock,
-        clientUUid,
+        clientUUid: uuid,
         init,
-        connected: false,
-    };
+        connected: true,
+    } satisfies Client;
+    clients.push(client);
+    return client;
 }
+
