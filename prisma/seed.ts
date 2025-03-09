700f15abf62f93c4ac1b202945ef337fc750e3c4
caiohvectora <caihebatista@gmail.com>
2025-03-08 23:19:58 -0300

    Refactor seed script to remove password hashing and improve code formatting

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,30 @@
-import { PrismaClient, type ContactCluster, type Contacts } from '@prisma/client';
+import {
+  PrismaClient,
+  type ContactCluster,
+  type Contacts,
+} from "@prisma/client";
 const prisma = new PrismaClient();
-import { genSalt, hash, compare } from 'bcrypt';
-import fs from 'fs/promises'
+import { genSalt, hash, compare } from "bcrypt";
+import fs from "fs/promises";
 async function seed() {
-    const user = await prisma.user.create({
-        data: {
-            email: 'Admin@gm.co',
-            password: await hash('Teste', await genSalt(10)),
-            name: 'Admin',
-        }
+  const user = await prisma.user.create({
+    data: {
+      email: "Admin@gm.co",
+      // password: await hash('Teste', await genSalt(10)),
+      name: "Admin",
+    },
+  });
+  const contacts = JSON.parse(
+    await fs.readFile(process.cwd() + "/prisma/data.json", {
+      encoding: "utf-8",
     })
-    const contacts = JSON.parse(await fs.readFile(process.cwd() + '/prisma/data.json', { encoding: 'utf-8' })) as { contacts: Contacts[], clusters: ContactCluster[] }
-    await prisma.contactCluster.createMany({
-        data: contacts.clusters
-    })
-    await prisma.contacts.createMany({
-        data: contacts.contacts
-    })
+  ) as { contacts: Contacts[]; clusters: ContactCluster[] };
+  await prisma.contactCluster.createMany({
+    data: contacts.clusters,
+  });
+  await prisma.contacts.createMany({
+    data: contacts.contacts,
+  });
 }
 
 seed();
