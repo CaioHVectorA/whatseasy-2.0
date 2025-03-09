22a54eb259c19d91d08d7bc2b82bfe8cb1f33cdb
caiohvectora <caihebatista@gmail.com>
2025-03-08 23:20:13 -0300

    Refactor index.ts for improved code formatting and organization

--- index.ts
+++ index.ts
@@ -1,99 +1,116 @@
-import Fastify from 'fastify'
-import websocket from '@fastify/websocket'
-import { main } from './src/lib/ws'
-import jwt, { } from '@fastify/jwt'
-import { userController } from '@/controllers/user.controller'
-import { authController } from '@/controllers/auth.controller'
-import { AppError } from '@/lib/appError'
-import { prisma } from '@/lib/prisma.client'
-import cors from '@fastify/cors'
-import { createClient, type Client } from '@/lib/wpp/Client'
-import { fstat, readdirSync, readSync } from 'fs'
-import type { User } from '@prisma/client'
-import { contactsController } from '@/controllers/contacts.controller'
-import { reactiveController } from '@/controllers/reactives.controller'
-import { connect } from '@/lib/wpp/connect'
+import Fastify from "fastify";
+import websocket from "@fastify/websocket";
+import { main } from "./src/lib/ws";
+import jwt from "@fastify/jwt";
+import { userController } from "@/controllers/user.controller";
+import { authController } from "@/controllers/auth.controller";
+import { AppError } from "@/lib/appError";
+import { prisma } from "@/lib/prisma.client";
+import cors from "@fastify/cors";
+import { createClient, type Client } from "@/lib/wpp/Client";
+import { fstat, readdirSync, readSync } from "fs";
+import type { User } from "@prisma/client";
+import { contactsController } from "@/controllers/contacts.controller";
+import { reactiveController } from "@/controllers/reactives.controller";
+import { connect } from "@/lib/wpp/connect";
 const fastify = Fastify({
-    logger: false
-})
-declare module 'fastify' {
-    interface FastifyRequest {
-        me: { id: string, email: string, name: string, last_connection: Date, isConnected: boolean };
-        clients: Client[];
-    }
+  logger: false,
+});
+declare module "fastify" {
+  interface FastifyRequest {
+    me: {
+      id: string;
+      email: string;
+      name: string;
+      last_connection: Date;
+      isConnected: boolean;
+    };
+    clients: Client[];
+  }
 }
 await fastify.register(cors, {
-    // put your options here
-    credentials: true,
-    origin: '*',
-})
+  // put your options here
+  credentials: true,
+  origin: "*",
+});
 const clients: Client[] = [];
 
-fastify.register(websocket)
-fastify.register(jwt, { secret: process.env.JWT_SECRET || "secret" })
-const port = process.env.PORT || 3333
-fastify.decorateRequest('clients', { getter: () => clients })
-fastify.register(authController)
+fastify.register(websocket);
+fastify.register(jwt, { secret: process.env.JWT_SECRET || "secret" });
+const port = process.env.PORT || 3333;
+fastify.decorateRequest("clients", { getter: () => clients });
+fastify.register(authController, { prefix: "/auth" });
 fastify.setErrorHandler(async (error, request, reply) => {
-    if (error instanceof AppError) {
-        reply.status(error.statusCode).send({
-            status: error.statusCode,
-            message: error.message,
-            isOperational: true,
-            // stack: error.stack
-        })
-    }
-    reply.status(error.statusCode || 500).send({ ...error, isOperational: false })
-})
+  if (error instanceof AppError) {
+    reply.status(error.statusCode).send({
+      status: error.statusCode,
+      message: error.message,
+      isOperational: true,
+      // stack: error.stack
+    });
+  }
+  reply
+    .status(error.statusCode || 500)
+    .send({ ...error, isOperational: false });
+});
 
 fastify.register(async function (fastify) {
-    fastify.get('/ws', { websocket: true }, (socket, req) => {
-        socket.on('connection', () => console.log('Client connected.'))
-        socket.on('message', main(socket, clients))
-        // socket.on('')
-    })
-})
+  fastify.get("/ws", { websocket: true }, (socket, req) => {
+    socket.on("connection", () => console.log("Client connected."));
+    socket.on("message", main(socket, clients));
+    // socket.on('')
+  });
+});
 fastify.addHook("onRequest", async (request, reply) => {
-    if (request.ws) return
-    if (request.url.startsWith('/auth')) return
-    try {
-        await request.jwtVerify()
-    } catch (err) {
-        reply.send(err)
-    }
-})
-fastify.get('/', async (request, reply) => { return { hello: 'world' } })
+  if (request.ws) return;
+  if (request.url.startsWith("/auth")) return;
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.send(err);
+  }
+});
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
 fastify.addHook("onRequest", async (request, reply) => {
-    if (request.ws || request.url.startsWith('/auth')) return
-    try {
-        // Supondo que request.user seja um JWT string
-        const user = request.user as { id: string };
-        // Busca o usuário pelo ID do JWT
-        //@ts-ignore
-        request.me = (await prisma.user.findUnique({
-            where: { id: user.id },
-            select: { id: true, email: true, name: true, Client: { select: { isConnected: true, last_conn: true } } }
-        }));
+  if (request.ws || request.url.startsWith("/auth")) return;
+  try {
+    // Supondo que request.user seja um JWT string
+    const user = request.user as { id: string };
+    // Busca o usuário pelo ID do JWT
+    //@ts-ignore
+    request.me = await prisma.user.findUnique({
+      where: { id: user.id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        Client: { select: { isConnected: true, last_conn: true } },
+      },
+    });
 
-        // Se o usuário não for encontrado, retorna erro
-        if (!request.me) {
-            return reply.status(404).send({ error: "Usuário não encontrado!" });
-        }
-    } catch (err) {
-        console.log({ err })
-        // Se der erro, retorna erro 500
-        return reply.status(500).send({ error: "Erro encontrando usuário!" });
+    // Se o usuário não for encontrado, retorna erro
+    if (!request.me) {
+      return reply.status(404).send({ error: "Usuário não encontrado!" });
     }
-})
-fastify.register(userController)
-fastify.register(contactsController)
-fastify.register(reactiveController)
-fastify.listen({
+  } catch (err) {
+    console.log({ err });
+    // Se der erro, retorna erro 500
+    return reply.status(500).send({ error: "Erro encontrando usuário!" });
+  }
+});
+fastify.register(userController);
+fastify.register(contactsController);
+fastify.register(reactiveController);
+fastify
+  .listen({
     port: Number(port),
-    host: process.env.HOST || '0.0.0.0'
-}).then(() => {
-    console.log('Server running at http://localhost:'+port)
-})
+    host: process.env.HOST || "0.0.0.0",
+  })
+  .then(() => {
+    console.log("Server running at http://localhost:" + port);
+  });
 // if (process.env.NODE_ENV) {
 //     readdirSync('auths').forEach((uuid) => createClient(uuid, clients))
 // }
