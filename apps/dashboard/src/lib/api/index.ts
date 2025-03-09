202d14afd2ee7e7c998dcfe95200a696907df046
caiohvectora <caihebatista@gmail.com>
2025-03-09 16:37:27 -0300

    Add token authorization to API requests and improve request transformation

--- apps/dashboard/src/lib/api/index.ts
+++ apps/dashboard/src/lib/api/index.ts
@@ -1,8 +1,21 @@
 import axios from 'axios';
+import { getCookie } from '../cookies';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json'
+  },
+  transformRequest: [
+    (d, h) => {
+      const token = getCookie('token');
+      if (token) {
+        h.Authorization = `Bearer ${token}`;
+      }
+      return JSON.stringify(d);
+    }
+  ],
+  validateStatus(status) {
+    return true;
   }
 });
