1348fc485330a97d398e981fa813ffb4d7d1cd5e
caiohvectora <caihebatista@gmail.com>
2025-03-09 23:45:48 -0300

    Add dashboard statistics for current and last month with dynamic rendering

--- apps/dashboard/src/pages/dashboard/index.tsx
+++ apps/dashboard/src/pages/dashboard/index.tsx
@@ -1,32 +1,92 @@
 import PageHead from '@/components/shared/page-head.jsx';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card';
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger
-} from '@/components/ui/tabs.js';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs.js';
 import RecentSales from './components/recent-sales.js';
 import { withUser } from '@/hooks/use-user.js';
 import { User } from '@/types/user.js';
+import { useQuery } from '@tanstack/react-query';
+import { api } from '@/lib/api/index.js';
+import { Send, Calendar, ChefHatIcon, Users } from 'lucide-react';
+type ApiResponse = {
+  currentMonth: {
+    Contacts: number;
+    Trigger: number;
+    SentMessages: number;
+    Schedule: number;
+  };
+  lastMonth: {
+    Contacts: number;
+    Trigger: number;
+    SentMessages: number;
+    Schedule: number;
+  };
+  clientSync: boolean;
+};
 
-function DashboardPage({ user }: {
-  user: User;
-}) {
-  console.log({ user })
+const dashboardData = [
+  {
+    title: 'Contatos',
+    value: 0,
+    change: 0,
+    icon: <Users className="h-4 w-4 text-muted-foreground" />
+  },
+  {
+    title: 'Reativos',
+    value: 0,
+    change: 0,
+    icon: <ChefHatIcon className="h-4 w-4 text-muted-foreground" />
+  },
+  {
+    title: 'Mensagens enviadas',
+    value: 0,
+    change: 0,
+    icon: <Send className="h-4 w-4 text-muted-foreground" />
+  },
+  {
+    title: 'Agendamentos',
+    value: 0,
+    change: 0,
+    icon: <Calendar className="h-4 w-4 text-muted-foreground" />
+  }
+];
+
+function DashboardCard({ title, value, change, icon }) {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className="text-2xl font-bold">{value}</div>
+        <p className="text-xs text-muted-foreground">{change} a mais que o mês passado!</p>
+      </CardContent>
+    </Card>
+  );
+}
+
+function DashboardPage({ user }: { user: User }) {
+  const { data } = useQuery({
+    queryKey: ['dashboard'],
+    queryFn: async () => {
+      const res = await api.get<{ data: ApiResponse }>('/user/initial-data');
+      return res.data.data
+    }
+  })
+  const dataToRender = !data ? dashboardData : dashboardData.map((item, index) => {
+    return {
+      ...item,
+      value: data.currentMonth[Object.keys(data.currentMonth)[index]],
+      change: data.currentMonth[Object.keys(data.currentMonth)[index]] - data.lastMonth[Object.keys(data.lastMonth)[index]]
+    }
+  })
   return (
     <>
       <PageHead title="Dashboard | App" />
       <div className="max-h-screen flex-1 space-y-4 overflow-y-auto p-4 pt-6 md:p-8">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
-            Olá, bem vindo de volta {user.name}👋
+            Olá, bem-vindo de volta {user.name}👋
           </h2>
         </div>
         <Tabs defaultValue="overview" className="space-y-4">
@@ -38,107 +98,9 @@ function DashboardPage({ user }: {
           </TabsList>
           <TabsContent value="overview" className="space-y-4">
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total Revenue
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">$45,231.89</div>
-                  <p className="text-xs text-muted-foreground">
-                    +20.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Subscriptions
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+2350</div>
-                  <p className="text-xs text-muted-foreground">
-                    +180.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <rect width="20" height="14" x="2" y="5" rx="2" />
-                    <path d="M2 10h20" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+12,234</div>
-                  <p className="text-xs text-muted-foreground">
-                    +19% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Active Now
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+573</div>
-                  <p className="text-xs text-muted-foreground">
-                    +201 since last hour
-                  </p>
-                </CardContent>
-              </Card>
+              {dataToRender.map((data, index) => (
+                <DashboardCard key={index} {...data} />
+              ))}
             </div>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7">
               <Card className="col-span-4">
@@ -150,9 +112,9 @@ function DashboardPage({ user }: {
               <Card className="col-span-4 md:col-span-3">
                 <CardHeader>
                   <CardTitle>Recent Sales</CardTitle>
-                  <CardDescription>
+                  <p className="text-xs text-muted-foreground">
                     You made 265 sales this month.
-                  </CardDescription>
+                  </p>
                 </CardHeader>
                 <CardContent>
                   <RecentSales />
