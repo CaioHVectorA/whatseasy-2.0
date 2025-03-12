15d1cd713095cfc226b71d51618ea99738393933
caiohvectora <caihebatista@gmail.com>
2025-03-12 20:56:31 -0300

    Refactor seed script to improve data handling and ensure user association for contacts and clusters

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,26 @@
-import {
-  PrismaClient,
-  type ContactCluster,
-  type Contacts,
-} from "@prisma/client";
+import { PrismaClient, type ContactCluster, type Contacts } from '@prisma/client';
 const prisma = new PrismaClient();
-import { genSalt, hash, compare } from "bcrypt";
-import fs from "fs/promises";
+import { genSalt, hash, compare } from 'bcrypt';
+import fs from 'fs/promises';
+import { data } from './data';
+const USER_ID = '2815e454-be3c-4812-aa91-808d66883cd6';
 async function seed() {
-  const user = await prisma.user.create({
-    data: {
-      email: "Admin@gm.co",
-      // password: await hash('Teste', await genSalt(10)),
-      name: "Admin",
-    },
-  });
   const contacts = JSON.parse(
-    await fs.readFile(process.cwd() + "/prisma/data.json", {
-      encoding: "utf-8",
+    await fs.readFile(process.cwd() + '/prisma/data.json', {
+      encoding: 'utf-8',
     })
   ) as { contacts: Contacts[]; clusters: ContactCluster[] };
+  await prisma.contactCluster.deleteMany({
+    where: { userId: USER_ID },
+  });
+  await prisma.contacts.deleteMany({
+    where: { userId: USER_ID },
+  });
   await prisma.contactCluster.createMany({
-    data: contacts.clusters,
+    data: contacts.clusters.map((cluster) => ({ ...cluster, userId: USER_ID, createdAt: new Date(), updatedAt: new Date() })),
   });
   await prisma.contacts.createMany({
-    data: contacts.contacts,
+    data: contacts.contacts.map((contact) => ({ ...contact, userId: USER_ID, createdAt: new Date(), updatedAt: new Date() })),
   });
 }
 
