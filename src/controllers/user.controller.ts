30fd2dbe57f7f3a4a11ad539a159b3a88fc6b1c2
caiohvectora <caihebatista@gmail.com>
2025-03-12 22:41:10 -0300

    Refactor user controller for improved readability and add functionality to retrieve sent messages by date

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,20 +1,18 @@
-import { prisma } from "@/lib/prisma.client";
-import type { Body } from "@/lib/types/utils";
-import type { Client } from "@/lib/wpp/Client";
-import { mountApiResponse } from "@/lib/ws/mount-response";
-import { type FastifyInstance, type FastifyPluginAsync } from "fastify";
+import { prisma } from '@/lib/prisma.client';
+import type { Body } from '@/lib/types/utils';
+import type { Client } from '@/lib/wpp/Client';
+import { mountApiResponse } from '@/lib/ws/mount-response';
+import { type FastifyInstance, type FastifyPluginAsync } from 'fastify';
 
-export const userController: FastifyPluginAsync = async (
-  fastify: FastifyInstance
-) => {
+export const userController: FastifyPluginAsync = async (fastify: FastifyInstance) => {
   // my fastify instance has a jwt plugin
-  fastify.get("/user/me", async (req, reply) => {
+  fastify.get('/user/me', async (req, reply) => {
     return mountApiResponse(req.me);
   });
-  fastify.get("/clients", async (req, reply) => {
+  fastify.get('/clients', async (req, reply) => {
     return mountApiResponse(req.clients.map((c) => c.clientUUid));
   });
-  fastify.patch<Body<{ name: string }>>("/user/name", async (req, reply) => {
+  fastify.patch<Body<{ name: string }>>('/user/name', async (req, reply) => {
     const { name } = req.body;
     const userAlreadyHasName = await prisma.user.findFirst({
       where: {
@@ -22,11 +20,7 @@ export const userController: FastifyPluginAsync = async (
       },
     });
     if (userAlreadyHasName?.name) {
-      return mountApiResponse(
-        {},
-        "Você já escolheu um nome",
-        "Você já escolheu um nome"
-      );
+      return mountApiResponse({}, 'Você já escolheu um nome', 'Você já escolheu um nome');
     }
     const user = await prisma.user.update({
       where: {
@@ -38,11 +32,15 @@ export const userController: FastifyPluginAsync = async (
     });
     return mountApiResponse(user);
   });
-  fastify.get("/user/initial-data", async (req, reply) => {
+  fastify.get<{
+    Querystring: {
+      month: string;
+    };
+  }>('/user/initial-data', async (req, reply) => {
     const currentMonth = new Date();
     const lastMonth = new Date();
     lastMonth.setMonth(currentMonth.getMonth() - 1);
-
+    const isMonth = req.query.month === 'true';
     const currentMonthData = await prisma.user.findUnique({
       where: {
         id: req.me.id,
@@ -51,44 +49,28 @@ export const userController: FastifyPluginAsync = async (
         Contacts: {
           where: {
             createdAt: {
-              gte: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              gte: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
         Trigger: {
           where: {
             createdAt: {
-              gte: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              gte: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
         SentMessages: {
           where: {
             createdAt: {
-              gte: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              gte: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
         Schedule: {
           where: {
             createdAt: {
-              gte: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              gte: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
@@ -104,11 +86,7 @@ export const userController: FastifyPluginAsync = async (
           where: {
             createdAt: {
               gte: new Date(lastMonth.getFullYear(), lastMonth.getMonth(), 1),
-              lt: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              lt: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
@@ -116,11 +94,7 @@ export const userController: FastifyPluginAsync = async (
           where: {
             createdAt: {
               gte: new Date(lastMonth.getFullYear(), lastMonth.getMonth(), 1),
-              lt: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              lt: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
@@ -128,11 +102,7 @@ export const userController: FastifyPluginAsync = async (
           where: {
             createdAt: {
               gte: new Date(lastMonth.getFullYear(), lastMonth.getMonth(), 1),
-              lt: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              lt: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
@@ -140,25 +110,15 @@ export const userController: FastifyPluginAsync = async (
           where: {
             createdAt: {
               gte: new Date(lastMonth.getFullYear(), lastMonth.getMonth(), 1),
-              lt: new Date(
-                currentMonth.getFullYear(),
-                currentMonth.getMonth(),
-                1
-              ),
+              lt: new Date(currentMonth.getFullYear(), currentMonth.getMonth(), 1),
             },
           },
         },
       },
     });
-    const clientExists = req.clients.find(
-      (c: Client) => c.clientUUid === req.me.id
-    );
+    const clientExists = req.clients.find((c: Client) => c.clientUUid === req.me.id);
     if (!currentMonthData || !lastMonthData) {
-      return mountApiResponse(
-        {},
-        "Erro ao buscar dados",
-        "Erro ao buscar dados"
-      );
+      return mountApiResponse({}, 'Erro ao buscar dados', 'Erro ao buscar dados');
     }
     const counts = {
       currentMonth: {
@@ -175,8 +135,25 @@ export const userController: FastifyPluginAsync = async (
       },
     };
 
+    const result = [];
+    // Query usando Prisma
+    const range = isMonth ? 30 : 7;
+    for (let i = 0; i < range; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() - i);
+      const messages = await prisma.sentMessages.findMany({
+        where: {
+          userId: req.me.id,
+          createdAt: {
+            gte: new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+            lt: new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1),
+          },
+        },
+      });
+      result.push({ date: date.toISOString(), messages: messages.length });
+    }
     if (!clientExists) {
-      return mountApiResponse({ ...counts, clientSync: false });
+      return mountApiResponse({ ...counts, sentMessages: result, clientSync: false });
     }
     // check if client is sync
     try {
@@ -186,17 +163,14 @@ export const userController: FastifyPluginAsync = async (
       // if (!clientSync) {
 
       // }
-      const clientSync = clientExists.sock.sendMessage(
-        "559992128746@s.whatsapp.net",
-        { text: "Olá, mundo!" }
-      );
-      return mountApiResponse({ ...counts, clientSync: true });
+      const clientSync = clientExists.sock.sendMessage('559992128746@s.whatsapp.net', { text: 'Olá, mundo!' });
+      return mountApiResponse({ ...counts, sentMessages: result, clientSync: true });
     } catch (err) {
-      console.log("DEU MERDA!!!!!!!!", err);
+      console.log('DEU MERDA!!!!!!!!', err);
       return mountApiResponse(
-        { ...counts, clientSync: false },
-        "Erro ao sincronizar com o cliente",
-        "Erro ao sincronizar com o cliente"
+        { ...counts, sentMessages: result, clientSync: false },
+        'Erro ao sincronizar com o cliente',
+        'Erro ao sincronizar com o cliente'
       );
     }
   });
